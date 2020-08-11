03eb1f15b9f33f7a0251f4639d4878d8ee5b5546
Sparsh Gautam <69500031+SparshGautam@users.noreply.github.com>
2020-08-11 04:24:47 -0700

    Create Calendar.py

---
+++ Calendar.py
@@ -0,0 +1,8 @@
+import calendar
+#Python has various importing options so to make our first calendar we must first import calendar to Python
+
+print(calendar.calendar(2020))
+# Print Fuction = it usualy specifies the output of out syntax Eg-print("hello world")
+#calendar Fuction = as we have imported calendar we use this function to specify that we are using this function
+#calendar.calendar = we are now using to get output as calendar we have to selec year here i have selected 2020
+#homework= now ry yourself to build the calendar for 3016
